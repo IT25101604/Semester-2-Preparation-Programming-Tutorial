6d446e8f6aea603b8163d294d90fb411a654b37f
IT25101604 <it25101604@my.sliit.lk>
2026-02-08 23:43:15 +0530

    Lab01 - Question01

---
+++ MarathonDistanceQ1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class MarathonDistanceQ1{
+	
+	public static void main(String args[]){
+		
+        // a) & b) Declare and initialize variables
+        int miles = 26;
+        int yards = 385;
+        double kilometers;
+
+        // c) & d) Calculate and save result
+        kilometers = (miles + (yards / 1768.0)) * 1.609;
+        System.out.println("Total Kilometers: " + kilometers);
+    }
+}	
+		
+		
